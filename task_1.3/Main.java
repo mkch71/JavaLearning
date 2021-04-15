94aa08b7438bec55a0d5cb3e30fd672463c426a4
Mike <62421801+mkch71@users.noreply.github.com>
2021-04-15 14:24:57 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,12 @@
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+       int a=14, b=1;
+       String s="SomeValue";
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(s);
+    }
+}
